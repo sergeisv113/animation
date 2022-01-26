0a6abb31f9542d2030dfc433c210ac330a8ebaad
Sergei <sergeisv113@gmail.com>
2022-01-26 14:11:27 +0200

    '3'

--- script.js
+++ script.js
@@ -9,8 +9,31 @@ const timerId = setTimeout(function (text) {
     console.log(text);
 }, 2000, 'hello');*/
 
-const timerId = setTimeout(logger, 2000);
-clearInterval(timerId);
+const  btn = document.querySelector('.btn');
+ let timerId;//запис чтобы использ вн ф-ции
+i = 0// колич раз запущ ф-ций
+//ф-ция анимации
+
+
+/*
+btn.addEventListener('click', () => {
+    // const timerId = setTimeout(logger, 2000);
+    const timerId = setInterval(logger, 500);
+
+})
+*/
+/*
 function logger() {
-    console.log('hello');
+    if (i === 3) {
+        clearInterval(timerId);
+    }
+    console.log('text');
+    i++
 };
+*/
+//рекурсивный вызов setTimeout.Ждет полное выполнен ф-ции
+/*
+let id = setTimeout(function log() {
+    console.log('Hello');
+    id = setTimeout(log, 500);
+}, 500);*/
